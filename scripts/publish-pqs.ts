a5335e2b42240aebd02f34d7ad4d4faed0e289e2
Ben Newman <ben@apollographql.com>
2024-08-08 14:10:50 -0400

    Support `npm run pq:publish` command.

---
+++ scripts/publish-pqs.ts
@@ -0,0 +1,28 @@
+import path from "node:path";
+import { spawnSync } from "node:child_process";
+import dotenv from "dotenv";
+
+const rootDir = path.dirname(__dirname);
+
+dotenv.config({
+  path: path.join(rootDir, ".env"),
+});
+
+const {
+  APOLLO_GRAPH_REF = "Shopify-hbkyeeh@chatbot",
+} = process.env;
+
+spawnSync(
+  "rover",
+  [
+    "persisted-queries",
+    "publish",
+    APOLLO_GRAPH_REF,
+    "--manifest", "operation-manifest.json",
+    "--profile", "shopify",
+  ],
+  {
+    stdio: "inherit",
+    cwd: rootDir,
+  }
+);
