d6ab53cc5422389c07d9910015a81cd87ec43e84
Michael Watson <xambrew@gmail.com>
2024-11-05 22:50:24 -0800

    Update PQ and mongodb tools

--- packages/ingest/src/ingest.config.ts
+++ packages/ingest/src/ingest.config.ts
@@ -1,4 +1,8 @@
-import { makeIngestMetaStore, Config } from "mongodb-rag-ingest";
+import {
+  makeIngestMetaStore,
+  Config,
+  IngestMetaStore,
+} from "mongodb-rag-ingest";
 import {
   makeOpenAiEmbedder,
   makeMongoDbEmbeddedContentStore,
@@ -9,6 +13,7 @@ import path from "path";
 import { loadEnvVars } from "./loadEnvVars";
 import { persistedQueryDataSource } from "./persistedQueryDataSource";
 import { MongoClient } from "mongodb";
+import { log } from "console";
 
 // Load project environment variables
 const dotenvPath = path.join(__dirname, "..", "..", "..", ".env"); // .env at project root
@@ -29,39 +34,44 @@ export default {
       deployment: OPENAI_EMBEDDING_MODEL,
       backoffOptions: {
         numOfAttempts: 25,
-        startingDelay: 1000,
+        startingDelay: 10,
       },
     });
   },
-  embeddedContentStore: async () => {
-    const embeddedContentStore = makeMongoDbEmbeddedContentStore({
+  embeddedContentStore: async () =>
+    makeMongoDbEmbeddedContentStore({
       connectionUri: MONGODB_CONNECTION_URI,
       databaseName: MONGODB_DATABASE_NAME,
-    });
-
-    const client = new MongoClient(MONGODB_CONNECTION_URI)
-    await client.connect();
-    await client.db(MONGODB_DATABASE_NAME).collection('embedded_content').deleteMany({});
-    await client.close();
-
-    return embeddedContentStore;
-  },
-  pageStore: () => 
+    }),
+  pageStore: () =>
     makeMongoDbPageStore({
       connectionUri: MONGODB_CONNECTION_URI,
       databaseName: MONGODB_DATABASE_NAME,
     }),
-  ingestMetaStore: () =>
-    makeIngestMetaStore({
-      connectionUri: MONGODB_CONNECTION_URI,
-      databaseName: MONGODB_DATABASE_NAME,
-      entryId: "all",
-    }),
+  ingestMetaStore: () => ({
+    entryId:"1",
+    loadLastSuccessfulRunDate: ()=>{
+      return new Promise((resolve) => {
+        resolve(new Date());
+    });
+    },
+    updateLastSuccessfulRunDate:()=>new Promise((r)=>r()),
+    close: ()=>new Promise((r)=>r())
+  }),
+  // () => ({
+  //   entryId:"",
+  //   loadLastSuccessfulRunDate: ()=>{
+  //     return new Promise(()=>{}).then(()=>Date.())
+  //   }
+  // })
+  // makeIngestMetaStore({
+  //   connectionUri: MONGODB_CONNECTION_URI,
+  //   databaseName: MONGODB_DATABASE_NAME,
+  //   entryId: "all",
+  // }),
   chunkOptions: () => ({
     transform: standardChunkFrontMatterUpdater,
   }),
   // Add data sources here
-  dataSources: async () => [
-    await persistedQueryDataSource(),
-  ],
+  dataSources: async () => [await persistedQueryDataSource()],
 } satisfies Config;
--- packages/ingest/src/persistedQueryDataSource.ts
+++ packages/ingest/src/persistedQueryDataSource.ts
@@ -3,7 +3,8 @@ import { readFileSync, readdirSync } from "node:fs";
 import { visit, parse as parseGraphQL } from "graphql";
 import { parse as parseYaml } from "yaml";
 import { DataSource } from "mongodb-rag-ingest/sources";
-import { Page } from "mongodb-rag-core";
+import { MongoClient, Page } from "mongodb-rag-core";
+import { loadEnvVars } from "./loadEnvVars";
 
 interface ManifestOperation {
   id: string;
@@ -13,7 +14,10 @@ interface ManifestOperation {
 }
 
 const rootDir = path.dirname(path.dirname(path.dirname(__dirname)));
-
+// Load project environment variables
+const dotenvPath = path.join(__dirname, "..", "..", "..", ".env"); // .env at project root
+const { MONGODB_CONNECTION_URI, MONGODB_DATABASE_NAME } =
+  loadEnvVars(dotenvPath);
 export async function persistedQueryDataSource(): Promise<DataSource> {
   const graphsDir = path.join(rootDir, "graphs");
 
@@ -23,16 +27,14 @@ export async function persistedQueryDataSource(): Promise<DataSource> {
       const pages: Page[] = [];
 
       for (const graphDirName of readdirSync(graphsDir)) {
-        if(graphDirName.includes(".DS")) continue;
+        if (graphDirName.includes(".DS")) continue;
         const graphDir = path.join(graphsDir, graphDirName);
         const manifestPath = path.join(graphDir, "operation-manifest.json");
         const manifest = JSON.parse(readFileSync(manifestPath, "utf-8"));
         const routerYamlPath = path.join(graphDir, "router.yaml");
         const routerYaml = readFileSync(routerYamlPath, "utf-8");
         const {
-          supergraph: {
-            listen: routerListenHost = "127.0.0.1:4000",
-          },
+          supergraph: { listen: routerListenHost = "127.0.0.1:4000" },
         } = parseYaml(routerYaml);
 
         manifest.operations.forEach((operation: ManifestOperation) => {
@@ -49,14 +51,12 @@ export async function persistedQueryDataSource(): Promise<DataSource> {
           });
 
           const metadata = {
-            id: operation.id,
+            id: operation.name,
             requiredVariables: Array.from(requiredVariablesSet),
             routerListenHost,
           };
 
-          const pseudoPageURL = `/persisted-queries/${
-            graphDirName
-          }/${operation.name}/?id=${metadata.id}`;
+          const pseudoPageURL = `/persisted-queries/${graphDirName}/${operation.name}/?id=${operation.name}`;
 
           pages.push({
             url: pseudoPageURL,
@@ -67,6 +67,27 @@ export async function persistedQueryDataSource(): Promise<DataSource> {
             metadata,
           });
         });
+      }
+
+      //If no operations, we want to reset the embedded content and db
+      //We drop conversations so we're not storing a lot of data
+      if (pages.length == 0) {
+        console.log("No operations found, resetting embeddings");
+        const client = new MongoClient(MONGODB_CONNECTION_URI);
+        await client.connect();
+        await client
+          .db(MONGODB_DATABASE_NAME)
+          .collection("embedded_content")
+          .deleteMany({});
+        await client
+          .db(MONGODB_DATABASE_NAME)
+          .collection("pages")
+          .deleteMany({});
+          await client
+          .db(MONGODB_DATABASE_NAME)
+          .collection("conversations")
+          .deleteMany({});
+        await client.close();
       }
 
       return pages;
